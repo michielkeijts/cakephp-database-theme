f67882e8e0a0e21a68995a105bd29b2de6662035
Michiel Keijts <michiel@normit.nl>
2023-01-31 10:13:10 +0100

    Fix error Declaration of SetupSeed::run() must be compatible with AbstactSeed run

--- config/Seeds/SetupSeed.php
+++ config/Seeds/SetupSeed.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @copyright (C) 2020 Michiel Keijts, Normit
- * 
+ *
  */
 
 use Migrations\AbstractSeed;
@@ -12,26 +12,26 @@ use Migrations\AbstractSeed;
  * @author michiel
  */
 class SetupSeed extends AbstractSeed {
-    
+
     /**
      * This restores a Default theme and places it at the top of the Hierarchy (MySQL related)
      */
-    public function run()
+    public function run(): void
     {
         $tableName = 'cake_database_themes_themes';
-    
+
         $table = $this->table($tableName);
-        
+
         $result = $this->fetchAll(sprintf("SELECT id FROM {$tableName} WHERE name = 'Default'"));
-        
+
         if (empty($result)) {
             $this->execute("UPDATE {$tableName} SET `lft` = `lft` + 1, `rght` = `rght` + 1");
             $this->execute("INSERT INTO {$tableName} (`name`, `lft`, `rght`) SELECT 'Default' AS `name`, 1 AS `lft`, (IFNULL(MAX(`rght`),1) + 1) AS `rght` FROM {$tableName}");
-            
+
             foreach ($this->fetchAll("SELECT id FROM {$tableName} WHERE `name` = 'Default' LIMIT 1") as $row) {
                 $this->execute("UPDATE {$tableName} SET parent_id = {$row['id']} WHERE parent_id IS NULL AND id != {$row['id']}");
             }
-            
+
         }
     }
 }
